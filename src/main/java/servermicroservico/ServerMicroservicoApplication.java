3ce69c8a917701e6c51c409516f5f960f8c05e5e
Caio Alexandre Pelarin Becker <caioapbecker@hotmail.com>
2023-11-21 21:02:49 -0300

    Initial commit

---
+++ src/main/java/servermicroservico/ServerMicroservicoApplication.java
@@ -0,0 +1,13 @@
+package servermicroservico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerMicroservicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerMicroservicoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/servermicroservico/ServerMicroservicoApplicationTests.java
@@ -0,0 +1,13 @@
+package servermicroservico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerMicroservicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
